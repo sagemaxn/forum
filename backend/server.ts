1931cd3d3d3a71c005eaa21fbd90d3517b65da1c
sagemaxn <sagemaxn@gmail.com>
2021-06-29 08:09:10 -0400

    modified:   backend/package-lock.json 	modified:   backend/package.json 	modified:   backend/server.ts 	new file:   backend/tsconfig.json

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,13 @@
-const { ApolloServer, gql } = require('apollo-server');
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+//const {ObjectType} = require('type-graphql')
+import * as Express from "express"
+import "reflect-metadata"
+
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema, Query, Resolver,  } from 'type-graphql'
 const User = require('./models/User.ts')
+
 
 require('dotenv').config()
 
@@ -19,67 +25,89 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true,
     console.log('error connection to MongoDB:', error.message)
   })
 
-const typeDefs = gql`
-    type User {
-        username: String!,
-        password: String
-    }
+@Resolver()
+class UserResolver {
+  @Query(() => String)
+  async hello() {
+    return "Hello World"
+  }
+  @Query(() => )
+}  
+// const typeDefs = gql`
+//     type User {
+//         username: String!,
+//         password: String
+//     }
+//     type Token {
+//       value: String!
+//     }
 
-    type Token {
-      value: String!
-    }
 
-type Query {
-    allUsers: [User!]!
-    findUser(username: String!): User
-}
-
-type Mutation {
-    createUser(username: String! password: String!): User
-    login(username: String! password: String!): Token
-}
-`
-const resolvers = {
-    Query: {
-      allUsers: async () => User.find()
-    },
-    Mutation: {
-      createUser: async (root, args) => {
-        console.log(args)
-        const password = await bcrypt.hash(args.password, 12)
-        console.log(password)
-        const user = await new User({...args, password});
-        console.log(user)
-        await user.save()
-       user.password = null;
-        return user;
-      },
-      login: async (root, args) => {
-        const user = await User.find({username: args.username})
-        console.log(user)
-        console.log(args.password)
-        console.log(user[0].password)
-        const password = await bcrypt.compare(args.password, user[0].password)
-        console.log(password)
-        if(user && password){
-          console.log('user logged in')
-          return "placeholder"
-        }
-        else{
-          console.log('invalid')
-          return null
-        }
-      }
-
-    }
-}
-
-const server = new ApolloServer({
-    typeDefs,
-    resolvers
-})
-
-server.listen().then(({ url }) => {
-    console.log(`Server ready at ${url}`)
+const main = async () => {
+  const schema = await buildSchema({
+    resolvers: [UserResolver]
   })
-  
+  const apolloServer = new ApolloServer({ schema });
+
+  const app = Express();
+
+  apolloServer.applyMiddleware({app});
+
+  app.listen(4000, () => {
+    console.log('Server started on 4000')
+  })
+}
+main()
+// const typeDefs = gql`
+//     type User {
+//         username: String!,
+//         password: String
+//     }
+//     type Token {
+//       value: String!
+//     }
+
+// type Query {
+//     allUsers: [User!]!
+//     findUser(username: String!): User
+// }
+
+// type Mutation {
+//     createUser(username: String! password: String!): User
+//     login(username: String! password: String!): Token
+// }
+// `
+// const resolvers = {
+//     Query: {
+//       allUsers: async () => User.find()
+//     },
+//     Mutation: {
+//       createUser: async (root, args) => {
+//         console.log(args)
+//         const password = await bcrypt.hash(args.password, 12)
+//         console.log(password)
+//         const user = await new User({...args, password});
+//         console.log(user)
+//         await user.save()
+//        user.password = null;
+//         return user;
+//       },
+//       login: async (root, args) => {
+//         const user = await User.find({username: args.username})
+//         console.log(user)
+//         console.log(args.password)
+//         console.log(user[0].password)
+//         const password = await bcrypt.compare(args.password, user[0].password)
+//         console.log(password)
+//         if(user && password){
+//           console.log('user logged in')
+//           return "placeholder"
+//         }
+//         else{
+//           console.log('invalid')
+//           return null
+//         }
+//       }
+
+//     }
+// }
