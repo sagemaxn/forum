19b9acad6da37a5ef451b4511985d84078faa36b
sagemaxn <sagemaxn@gmail.com>
2022-06-26 22:28:20 -0400

    modified:   backend/src/PostResolver.ts 	modified:   frontend/src/components/ChangeAvatar.tsx

--- frontend/src/components/ChangeAvatar.tsx
+++ frontend/src/components/ChangeAvatar.tsx
@@ -3,7 +3,6 @@ import {
   ModalOverlay,
   ModalContent,
   ModalHeader,
-  ModalFooter,
   ModalBody,
   ModalCloseButton,
   Button,
@@ -12,107 +11,96 @@ import {
   Box,
   HStack,
   FormControl,
-  FormLabel
 } from "@chakra-ui/react";
+import { useState } from 'react'
 import { Formik, Form, Field } from "formik";
-import { userInfo } from "os";
 
 import { useChangeAvatarMutation } from "../generated/graphql";
 
-function RadioCard(props) {
-  const { getInputProps, getCheckboxProps } = useRadio(props);
+function ChangeAvatar({ isOpen, onClose, avatar, user }) {
+  const [val, setVal] = useState('')
 
-  const input = getInputProps();
-  const checkbox = getCheckboxProps();
-
-  return (
-    <Box as="label">
-      <input {...input} />
-      <Box
-        {...checkbox}
-        cursor="pointer"
-        borderWidth="3px"
-        borderRadius="md"
-        boxShadow="md"
-        _checked={{
-          borderColor: "teal.600",
-        }}
-        _focus={{
-          boxShadow: "outline",
-        }}
-        px={5}
-        py={3}
-      >
-        {props.children}
+  function CustomRadio(props) {
+    const { getInputProps, getCheckboxProps } = useRadio(props);
+    const input = getInputProps();
+    const checkbox = getCheckboxProps();
+    return (
+      <Box as="label">
+        <input {...input} />
+        <Box {...checkbox} _checked={{ color: "red" }} >
+          {props.children}
+        </Box>
       </Box>
-    </Box>
-  );
-}
+    );
+  }
+  
+  function FormField({ name, children, ...rest }, props) {
 
-function Radio({username, avatar}) {
-  const options = ["default", "star", "heart"];
-  const [change, { data }] = useChangeAvatarMutation()
+    return (
+      <Field name='avatar'>
+        {({ field, form }) => (
+          <FormControl>
+            <CustomRadio  {...field} id={name}>{children}</CustomRadio>
+          </FormControl>
+        )}
+      </Field>
+    )
+  }
+  function handleChange(value) {
+    console.log(value)
+    setVal(value)
+  }
 
-  const { getRootProps, getRadioProps } = useRadioGroup({
-    name: "framework",
-    defaultValue: "react",
-    onChange: console.log,
-  });
+  function Group() {
+    const [change, { data }] = useChangeAvatarMutation()
+    const avatars = [
+      { name: 'default' },
+      { name: 'star' },
+      { name: 'heart' },
+    ];
+    const { value, getRootProps, getRadioProps } = useRadioGroup({
+      defaultValue: avatar,
+      onChange: handleChange
+    });
 
-  const group = getRootProps();
+    const group = getRootProps();
+    return (
+      <Formik
+        initialValues={{ username: user, avatar: val }}
+        onSubmit={async (values, actions) => {
 
-  return (
-    <Formik
-      initialValues={{ username, avatar}}
-      onSubmit={async (values, actions) => {
-        console.log(values, '1')
-        await change({ variables: values});
-        if (data) {
-          console.log(data);
-        }
-        actions.setSubmitting(false);
-      }}
-    >
-      {(props) => (
-        <>
-          <HStack {...group}>
-            <Form>
-              <Field>
-                {({ field, form }) => (
-                  <FormControl>
-                    <FormLabel htmlFor="avatar">avatar</FormLabel>
-
-                    {options.map((value) => {
-                      console.log(value)
-                      const radio = getRadioProps({ value });
-                      return (
-                        <RadioCard key={value} {...radio}>
-                          {value}
-                        </RadioCard>
-                      );
-                    })}
-                  </FormControl>
-                )}
-              </Field>
-
+          console.log(values)
+          await change({ variables: values });
+          if (data) {
+            console.log(data)
+          }
+          actions.setSubmitting(false);
+        }}>
+        {(props) => (
+          <Form>
+            <HStack {...group}>
+              {avatars.map((avatar) => (
+                <FormField name={'avatar'} key={avatar.name}>
+                  <CustomRadio {...getRadioProps({ value: avatar.name })}>
+                    {avatar.name}
+                  </CustomRadio>
+                </FormField>
+              ))}
               <Button
                 mt={4}
                 backgroundColor="green"
-                w="100%"
                 isLoading={props.isSubmitting}
                 type="submit"
               >
-                Post
+                Save
               </Button>
-            </Form>
-          </HStack>
-        </>
-      )}
-    </Formik>
-  );
-}
+            </HStack>
+          </Form>
+        )}
+      </Formik>
+    );
+  }
 
-const ChangeAvatar = ({ isOpen, onClose, avatar, user }) => {
   return (
     <>
       <Modal isOpen={isOpen} onClose={onClose}>
@@ -121,21 +109,12 @@ const ChangeAvatar = ({ isOpen, onClose, avatar, user }) => {
           <ModalHeader>Pick New Avatar</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <Radio username={user} avatar={avatar}/>
+            <Group />
           </ModalBody>
-
-          <ModalFooter>
-            <Button mr={3} onClick={onClose}>
-              cancel
-            </Button>
-            <Button mr={3} onClick={onClose}>
-              save
-            </Button>
-          </ModalFooter>
         </ModalContent>
       </Modal>
     </>
   );
-};
+}
 
-export default ChangeAvatar;
+export default ChangeAvatar
