67c586b69163d8b4a835030484b173560a879f7d
sagemaxn <sagemaxn@gmail.com>
2022-08-10 22:23:48 -0400

    C

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,36 +1,39 @@
 import { useState, useEffect } from "react";
 import { Formik, Form, Field } from "formik";
-import {
-  Button,
-  Input,
-  FormLabel,
-  FormControl,
-  FormErrorMessage,
-  Link,
-  Box
-} from "@chakra-ui/react";
+import { Button, Link, Box, Alert, AlertDescription, AlertIcon, AlertTitle } from "@chakra-ui/react";
 import { HiEye, HiEyeOff } from "react-icons/hi";
 
 import FormField from "../components/FormField";
+<<<<<<< Updated upstream
 import { useLoginMutation} from "../generated/graphql";
 import {useRouter} from 'next/router'
+=======
+import { useLoginMutation } from "../generated/graphql";
+import { useRouter } from "next/router";
+>>>>>>> Stashed changes
 
-function LoginForm({setForm, form}) {
-  const [login, { data }] = useLoginMutation({ onCompleted({login}){
-   if(data){ console.log(data)}
-  }});
+function LoginForm({ setForm, form }) {
+
+const [hidden, setHidden] = useState(true)
+  const [login, { data }] = useLoginMutation({
+    onCompleted({ login }) {
+      if (data) {
+        console.log(data);
+      }
+    },
+  });
 
   const [toggle, setToggle] = useState(true);
-  const [boolean, setBoolean] = useState(true)
+  const [boolean, setBoolean] = useState(true);
 
   interface valuesInt {
     password: string;
     username: string;
   }
 
-  if (data && boolean){
-    if(data.login.token !== 'no token'){
-      useRouter().push('/')
+  if (data && boolean) {
+    if (data.login.token !== "no token") {
+      useRouter().push("/");
     }
   }
   return (
@@ -44,17 +47,20 @@ function LoginForm({setForm, form}) {
             useRouter().push('/')
           }
         }
+        else setHidden(false)
         actions.setSubmitting(false);
+        
       }}
     >
       {(props) => (
         <Form>
-          {/* <Button onClick={() => }/> */}
           <FormField name="username"></FormField>
-          <FormField name="password" toggle={toggle}>      <Box onClick={() => setToggle(!toggle)} zIndex='3'>
-            {toggle ? <HiEyeOff/> : <HiEye/>}
-          </Box></FormField>
-    
+          <FormField name="password" toggle={toggle}>
+            {" "}
+            <Box onClick={() => setToggle(!toggle)} zIndex="3">
+              {toggle ? <HiEyeOff /> : <HiEye />}
+            </Box>
+          </FormField>
 
           <Button
             mt={4}
@@ -64,7 +70,14 @@ function LoginForm({setForm, form}) {
           >
             Login
           </Button>
-          <Link onClick={(()=> setForm(''))}>Back</Link>
+          <Link onClick={() => setForm("")}>Back</Link>
+          <Alert status="error" hidden={hidden}>
+            <AlertIcon />
+            <AlertTitle>Wrong username and/or password</AlertTitle>
+            <AlertDescription>
+              
+            </AlertDescription>
+          </Alert>
         </Form>
       )}
     </Formik>
