74d1e61fc61839378ef6856c4ffec7c50ccc938a
sagemaxn <sagemaxn@gmail.com>
2022-09-03 07:02:03 -0400

    modified:   frontend/src/pages/user/[...slug].tsx

--- frontend/src/pages/user/[...slug].tsx
+++ frontend/src/pages/user/[...slug].tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { useFindUserQuery, FindUserDocument } from "../../generated/graphql";
-import { initializeApollo } from "../../lib/apollo";
 import auth from "../../lib/auth";
 import { compose } from "../../lib/compose";
 import UserProfile from '../../components/UserProfile'
@@ -11,17 +10,19 @@ const User = ({ decoded }) => {
   const router = useRouter();
   const { slug } = router.query;
   const [user, setUser] = useState('no user found')
-  const { data, loading, error } = useFindUserQuery({
-    variables: {
-      offset: offset,
-      limit: 5
-    }
-  });
+  let offset = 1
   if(typeof slug === 'string'){
-    if (user !== slug)
-    setUser(slug)
-  }
-
+    if (user !== slug){
+    setUser(slug)}
+    if(parseInt(slug, 10) ){
+      offset = parseInt(slug) * 5
+  
+      const { data, loading, error } = useFindUserQuery({
+        variables: {
+          offset: offset,
+          limit: 5
+        }
+      });
   if (loading) return <Navbar user={decoded.user} avatar={decoded.avatar}/>
   if(data){
       console.log(data)
@@ -31,6 +32,8 @@ const User = ({ decoded }) => {
   }
   return<>  <Navbar user={decoded.user} avatar={decoded.avatar}/> <div>404 no user found</div> </>
 };
+}
+}
 
 export const getServerSideProps = compose(auth);
 
